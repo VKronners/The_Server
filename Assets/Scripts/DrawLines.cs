1be71dd639c8263c444f9b00fe65471f2d5341db
Victor Kronner <V.Kronner@outlook.com>
2018-06-05 00:08:48 -0300

    The Server
    FirstVersion The Server

---
+++ Assets/Scripts/DrawLines.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DistanciaMaximaDasLinhas
+{
+    Pequena = 5,
+    Mediana = 10,
+    Maxima = 15
+}
+
+public class DrawLines : MonoBehaviour {
+
+    
+}
---
+++ Assets/Scripts/SpawnLittleNodes.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TamanhoDoJogo
+{
+    Pequeno = 30,
+    Medio = 60,
+    Grande = 90,
+    Colossal = 300,
+}
+public enum EspacamentoDosNodos
+{
+    Pequeno = 15,
+    Medio = 20,
+    Grande = 25,
+    Colossal = 30
+}
+
+public class SpawnLittleNodes : MonoBehaviour {
+
+    DrawLines drawLines;
+
+    public GameObject prefabLittleNode;
+    public GameObject mainNode;
+
+    [SerializeField]
+    EspacamentoDosNodos espacamentoDosNodos;
+    [SerializeField]
+    TamanhoDoJogo tamanhoDoJogo;
+ 
+    Vector3 posicaoNoMundoLittleNode;
+
+    Transform NodoPai;
+
+    void Start () {
+
+        NodoPai = GameObject.Find("Nodo Pai").GetComponent<Transform>();
+        drawLines = this.gameObject.GetComponent<DrawLines>();
+
+        for (int i = 0; i < (int)tamanhoDoJogo; i++)
+        {
+            posicaoNoMundoLittleNode = Random.insideUnitSphere * (int)espacamentoDosNodos;
+
+            if (Vector3.Distance(posicaoNoMundoLittleNode, mainNode.transform.position) <= 4)
+            {
+                posicaoNoMundoLittleNode = Random.insideUnitSphere * (int)espacamentoDosNodos;
+            }
+            else
+            {
+                drawLines.allNodes.Add(Instantiate(prefabLittleNode, posicaoNoMundoLittleNode, Quaternion.identity, NodoPai));
+            }
+            if (i == (int)tamanhoDoJogo)
+            {
+                for (i = 0; i < (int)tamanhoDoJogo; i++) {
+                    //float distancia = Vector3.Distance(Nodos.transform.position, drawLines.allNodes[i].transform.position;
+                }
+            }
+            if (prefabLittleNode.GetComponent<DrawLines>() == null)
+            {
+                prefabLittleNode.AddComponent<DrawLines>();
+            }
+
+        }
+    }
+	
+	void Update () {
+		
+	}
+}
